11e878d2a220f036ec3fd82dc6780266e9e57dec
SohamDesai1 <sohamdesai123@gmail.com>
2026-04-26 12:11:19 +0530

    feat: :zap: updated nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,2 @@
-if vim.g.vscode then
-    -- VSCode Neovim
-    require "user.vscode_keymaps"
-    else
-        -- Ordinary Neovim
-        end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
